cfbe9e7dc9d2b31983358cff95001cd0ad550eee
Ynda Jas <yndajas@gmail.com>
2021-05-20 21:05:24 +0100

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,23 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie, index) => (
+              <div key={index}>
+                <h2>{movie.title}</h2>
+                <p>{movie.time} minutes</p>
+                <p>Genres:</p>
+                <ul>
+                  {movie.genres.map((genre, index) => (
+                        <li key={index}>{genre}</li>
+                      )
+                    )
+                  }
+                </ul>
+              </div>
+            )
+          )
+        }
     </div>
   );
 };
